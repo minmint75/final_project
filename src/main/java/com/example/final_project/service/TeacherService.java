9449ca90830c0ba3c9f1676b92dc291eae6b479d
Mint <demonheart752005@gmail.com>
2025-11-04 21:18:32 +0700

    teacher service

---
+++ src/main/java/com/example/final_project/service/TeacherService.java
@@ -0,0 +1,15 @@
+package com.example.final_project.service;
+
+import com.example.final_project.entity.Teacher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.util.Optional;
+
+public interface TeacherService {
+
+    Page<Teacher> searchTeachers(String keyword, Pageable pageable);
+    Optional<Teacher> findById(Long id);
+    Teacher save(Teacher teacher);
+    void deleteById(Long id);
+    Optional<Teacher> findByUsername(String username);
+}
