591c70b5deef5d0d8002fcde6b184ad01d76b27b
Nhungvu439 <146834474+nhungvu05@users.noreply.github.com>
2025-11-09 01:17:17 +0700

    Tạo nhánh Categor Request

---
+++ src/main/java/com/example/final_project/dto/CategoryRequest.java
@@ -0,0 +1,21 @@
+package com.example.final_project.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryRequest {
+
+    private Long id; // dùng khi update, null khi tạo mới
+
+    @NotBlank(message = "Tên danh mục không được để trống")
+    @Size(max = 100, message = "Tên danh mục không được vượt quá 100 ký tự")
+    private String name;
+
+    @Size(max = 255, message = "Mô tả không được vượt quá 255 ký tự")
+    private String description;
+
+}
