0768e8f1eabc3569ec32afe181b3141a54d9f8a2
Thuy Hien <hiwn1222@gmail.com>
2025-11-18 19:31:34 +0700

    fix controller

--- src/main/java/com/example/final_project/controller/ExamHistoryController.java
+++ src/main/java/com/example/final_project/controller/ExamHistoryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/exam-history")
+@RequestMapping("exam-history")
 @RequiredArgsConstructor
 public class ExamHistoryController {
 
