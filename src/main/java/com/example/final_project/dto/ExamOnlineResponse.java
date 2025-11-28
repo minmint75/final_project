1cbc51029af77bbeef3963801fdea781fde252d9
vtnhung <vuthinhung061005@gmail.com>
2025-11-28 16:27:17 +0700

    examonlineresponse

---
+++ src/main/java/com/example/final_project/dto/ExamOnlineResponse.java
@@ -0,0 +1,22 @@
+package com.example.final_project.dto;
+
+import com.example.final_project.entity.ExamLevel;
+import com.example.final_project.entity.ExamStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ExamOnlineResponse {
+    private Long id;
+    private String name;
+    private Integer numberOfQuestions;
+    private ExamLevel level;
+    private LocalDateTime submissionDeadline;
+    private Integer passingScore;
+    private Integer maxParticipants;
+    private String accessCode;
+    private ExamStatus status;
+    private String teacherName;
+    private LocalDateTime createdAt;
+}
