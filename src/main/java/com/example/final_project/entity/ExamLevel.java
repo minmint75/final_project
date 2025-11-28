024ca42b2fdd99b84854a19ef395e11e6ede1b10
vtnhung <vuthinhung061005@gmail.com>
2025-11-28 16:25:36 +0700

    examlevel

---
+++ src/main/java/com/example/final_project/entity/ExamLevel.java
@@ -0,0 +1,10 @@
+package com.example.final_project.entity;
+
+/**
+ * Defines the difficulty levels for an exam.
+ */
+public enum ExamLevel {
+    DE,         // Easy
+    TRUNG_BINH, // Medium
+    KHO         // Hard
+}
