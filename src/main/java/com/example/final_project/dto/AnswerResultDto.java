e386572d782e1e8a3463c904b35c4f2fc8cb163d
Mint <demonheart752005@gmail.com>
2025-11-30 01:28:51 +0700

    detail

---
+++ src/main/java/com/example/final_project/dto/AnswerResultDto.java
@@ -0,0 +1,15 @@
+package com.example.final_project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnswerResultDto {
+    private Long id;
+    private String text;
+    private boolean correct;
+    private boolean selected;
+}
