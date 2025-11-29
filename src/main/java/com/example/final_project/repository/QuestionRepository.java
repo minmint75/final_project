0f68b998fac6b4aa4a1751279382ddc678c0b25f
Nhungvu439 <146834474+nhungvu05@users.noreply.github.com>
2025-11-30 02:06:18 +0700

    cập nhật 4

--- src/main/java/com/example/final_project/repository/QuestionRepository.java
+++ src/main/java/com/example/final_project/repository/QuestionRepository.java
@@ -3,8 +3,10 @@ package com.example.final_project.repository;
 import com.example.final_project.entity.Question;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     Page<Question> findAllByOrderByCreatedAtDesc(Pageable pageable);
     Page<Question> findByCreatedByOrderByCreatedAtDesc(String createdBy, Pageable pageable);
+    List<Question> findByDifficulty(String difficulty);
 }
