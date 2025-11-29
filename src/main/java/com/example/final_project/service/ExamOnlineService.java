db1a90b36847709bccd7916ec1b90169babff4bc
Nhungvu439 <146834474+nhungvu05@users.noreply.github.com>
2025-11-30 03:40:59 +0700

    fix 4

--- src/main/java/com/example/final_project/service/ExamOnlineService.java
+++ src/main/java/com/example/final_project/service/ExamOnlineService.java
@@ -3,17 +3,20 @@ package com.example.final_project.service;
 import com.example.final_project.dto.ExamOnlineRequest;
 import com.example.final_project.dto.ExamOnlineResponse;
 import com.example.final_project.dto.ExamOnlineResultsDto;
+import org.springframework.security.core.Authentication;
+
 import java.util.List;
 
 public interface ExamOnlineService {
 
-    ExamOnlineResponse createExamOnline(ExamOnlineRequest request, Long teacherId);
-    ExamOnlineResponse startExamOnline(Long examOnlineId, Long teacherId);
-    ExamOnlineResultsDto getExamOnlineResults(Long examOnlineId, Long teacherId);
-    List<ExamOnlineResponse> getMyOnlineExams(Long teacherId);
-    ExamOnlineResponse getExamOnlineById(Long examOnlineId, Long teacherId);
-    ExamOnlineResponse updateExamOnline(Long id, ExamOnlineRequest request, Long teacherId);
-    ExamOnlineResponse finishExamOnline(Long examOnlineId, Long teacherId);
-    void deleteExamOnlineById(Long examOnlineId, Long teacherId);
+    ExamOnlineResponse createExamOnline(ExamOnlineRequest request, Authentication authentication);
+    ExamOnlineResponse startExamOnline(Long examOnlineId, Authentication authentication);
+    ExamOnlineResultsDto getExamOnlineResults(Long examOnlineId, Authentication authentication);
+    List<ExamOnlineResponse> getMyOnlineExams(Long teacherId); // Keep as is, logic is in controller
+    ExamOnlineResponse getExamOnlineById(Long examOnlineId, Authentication authentication);
+    ExamOnlineResponse updateExamOnline(Long id, ExamOnlineRequest request, Authentication authentication);
+    ExamOnlineResponse finishExamOnline(Long examOnlineId, Authentication authentication);
+    void deleteExamOnlineById(Long examOnlineId, Authentication authentication);
+    List<ExamOnlineResponse> getAllOnlineExams();
 
 }
